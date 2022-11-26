c90bc075de6803737f7cacc0d0fa26f6e1e77bd1
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-11-26 02:13:26 -0500

    Create Find_the_position.py

---
+++ 8th Kyu/Find_the_position.py
@@ -0,0 +1,8 @@
+#When provided with a letter, return its position in the alphabet.
+
+#Input :: "a"
+
+#Ouput :: "Position of alphabet: 1"
+
+def position(alphabet):
+    return f"Position of alphabet: {ord(alphabet.lower())-96}"
